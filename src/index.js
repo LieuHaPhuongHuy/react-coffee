cb2219c4c783f34e22ddb446abc0cd828673a8cc
NNewbieDev <phuonghuy.4444@gmail.com>
2025-09-04 14:01:58 +0700

    update base name router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
      // <React.StrictMode>
      <ContextProvider>
-          <BrowserRouter >
+          <BrowserRouter basename="react-coffee" >
                <App />
           </BrowserRouter>
      </ContextProvider>
